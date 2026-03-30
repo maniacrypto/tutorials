2b78fbb69c6588f61d87f02d1cb00d17384b0c76
maniacrypt <maniacrypto97@gmail.com>
2026-03-30 20:17:06 +0500

    tester

---
+++ testdata.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract FunctionModifier {
+
+    address public owner;
+    uint256 public x = 10;
+
+
+    constructor() {
+
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+
+        _;
+    }
+
+    modifier validAddress(address _addr) {
+        require(_addr != address(0), "Not valid address");
+        _;
+    }
+
+    function changeOwner(address _newOwner)
+        public
+        onlyOwner
+        validAddress(_newOwner)
+    {
+        owner = _newOwner;
+        
+    }
