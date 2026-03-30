34943e932e8324fc3ff28a1fca3b552ec6f234b4
maniacrypt <maniacrypto97@gmail.com>
2026-03-31 01:11:17 +0500

    cons

---
+++ contructor.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+// Base contract X
+contract X {
+    string public name;
+
+    constructor(string memory _name) {
+        name = _name;
+    }
+}
+
+// Base contract Y
+contract Y {
+    string public text;
+
+    constructor(string memory _text) {
+        text = _text;
+    }
+}
